ede4e4a58751451f0ccf361dc8cd1e793b68e55c
Joshua Vining <joshuavining86@gmail.com>
2020-06-22 13:28:00 -0400

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// Enable Tool Tips Globally 
+$(function () {
+	$('[data-toggle="tooltip"]').tooltip()
+});
+
+
+// Radio Button Selector * FOR DEVELOPMENT ONLY *
+$(document).ready(function () {
+    $('input:radio').click(function () {
+        var selValue = $("input:radio:checked").val();
+		$("header .col-md-12").removeClass().addClass("col-md-12").addClass(selValue);
+		if ($('header .col-md-12').hasClass('text-center')) { $(".col-md-5").first().removeClass().addClass("col-md-5 offset-md-1 mt-3")};
+		if ($('header .col-md-12').hasClass('text-right')) { $(".col-md-5" ).first().removeClass().addClass("col-md-5 offset-md-2 mt-3")};
+		if ($('header .col-md-12').hasClass('text-left')) {	$(".col-md-5").first().removeClass().addClass("col-md-5 mt-3")};
+    });
+});
+// END Radio Button Selector * FOR DEVELOPMENT ONLY *
