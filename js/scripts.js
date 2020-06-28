abae63bf7473666069eed19d4f4131d1db7a03d8
Joshua Vining <joshuavining86@gmail.com>
2020-06-28 19:58:58 -0400

    Script Compensates for Hero Marketing Image

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,18 @@ $(document).ready(function () {
     $('input:radio').click(function () {
         var selValue = $("input:radio:checked").val();
 		$("header .col-md-12").removeClass().addClass("col-md-12").addClass(selValue);
-		if ($('header .col-md-12').hasClass('text-center')) { $(".col-md-5").first().removeClass().addClass("col-md-5 offset-md-1 mt-3")};
-		if ($('header .col-md-12').hasClass('text-right')) { $(".col-md-5" ).first().removeClass().addClass("col-md-5 offset-md-2 mt-3")};
-		if ($('header .col-md-12').hasClass('text-left')) {	$(".col-md-5").first().removeClass().addClass("col-md-5 mt-3")};
+		if ($('header .col-md-12').hasClass('text-center')) {
+			$(".col-md-5").first().removeClass().addClass("col-md-5 offset-md-1 mt-3");
+			$("header img").first().removeClass().addClass("img-fluid mx-auto d-block");
+		};
+		if ($('header .col-md-12').hasClass('text-right')) { 
+			$(".col-md-5" ).first().removeClass().addClass("col-md-5 offset-md-2 mt-3");
+ 			$("header img").first().removeClass().addClass("img-fluid ml-auto d-block");
+		};
+		if ($('header .col-md-12').hasClass('text-left')) {	
+			$(".col-md-5").first().removeClass().addClass("col-md-5 mt-3");
+			$("header img").first().removeClass().addClass("img-fluid mr-auto d-block");
+		};
     });
 });
 // END Radio Button Selector * FOR DEVELOPMENT ONLY *
